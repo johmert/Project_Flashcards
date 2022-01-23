902a1d60f58ff4a361657f9a11fd67598bfe0d85
johmert <johmert@users.noreply.github.com>
2022-01-22 22:00:58 -0800

    route to DeckList

---
+++ src/Layout/deck/DeckList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function DeckList() {
+    return <h1>This is where the DeckList will be</h1>
+}
+
+export default DeckList;
--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
 import Header from "./Header";
 import NotFound from "./NotFound";
+import DeckList from "./deck/DeckList";
 
 function Layout() {
+  const { path } = useRouteMatch();
+
   return (
     <>
       <Header />
       <div className="container">
         {/* TODO: Implement the screen starting here */}
-        <NotFound />
+        <Switch>
+          <Route path={path}>
+            <DeckList />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
       </div>
     </>
   );
