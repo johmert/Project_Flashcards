aa3eaa26384880a1f4d16f89b01bc10957541f20
johmert <johmert@users.noreply.github.com>
2022-01-25 21:15:39 -0800

    Deck reads

--- src/Layout/deck/Deck.js
+++ src/Layout/deck/Deck.js
@@ -1,15 +1,34 @@
-import React from "react";
-import {Route, Switch} from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {Route, Switch, useParams} from "react-router-dom";
 import Breadcrumb from "../Breadcrumb";
+import {readDeck} from "../../utils/api/index";
 
-function Deck({deck}) {
+function Deck() {
+    const [deck, setDeck] = useState({}); 
+    const {deckId} = useParams();
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+
+    useEffect(() => {
+        async function getDeck() {
+          const response = await readDeck(deckId, signal);
+          setDeck(response);
+        }
+        getDeck();
+        return () => {
+          abortController.abort();
+        }
+      }, {});
+    
     return (
-        <div id={`deck-${deck.id}`}>
+        <div id={`deck-${deckId}`}>
             <Switch>
                 <Route>
                     <Breadcrumb deck={deck}/>
                 </Route>
             </Switch>
+            <h1>{deck.name}</h1>
+            <h6>{deck.description}</h6>
         </div>
     );
 }
