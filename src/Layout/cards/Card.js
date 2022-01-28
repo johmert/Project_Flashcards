d68ac593c8d4af62b6a84f3437529f26d76ced4b
johmert <johmert@users.noreply.github.com>
2022-01-27 23:22:46 -0800

    refactoring

--- src/Layout/cards/Card.js
+++ src/Layout/cards/Card.js
@@ -5,38 +5,12 @@ import Breadcrumb from "../Breadcrumb";
 import EditCard from "./EditCard";
 
 function Card({deck, deckId}){
-    const [card, setCard] = useState({});
-    const cardId = useParams().cardId;
-    const abortController = new AbortController();
-    const signal = abortController.signal;
 
-    async function getCard(){
-        try {
-            const response = await readCard(cardId, signal);
-            setCard(response);
-        } catch(error){
-            if(error.name !== "AbortError") {
-                throw error;
-            }
-        }
-    }
-
-    useEffect(() => {
-        getCard();
-        return () => {
-            abortController.abort();
-        }
-    }, []);
     
 
     return (
         <div>
-            <Breadcrumb deck={deck} card={card}/>
-            <Switch>
-                <Route path={`/decks/${deckId}/cards/:cardId/edit`}>
-                    <EditCard card={card} deckId={deckId}/>
-                </Route>
-            </Switch>
+
         </div>
     );
 }
--- src/Layout/cards/CardPreview.js
+++ src/Layout/cards/CardPreview.js
@@ -1,16 +1,50 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {Route, Switch, useHistory} from "react-router-dom";
+import { readCard, updateCard } from "../../utils/api";
 import Delete from "../buttons/Delete";
 import Edit from "../buttons/Edit";
+import EditCard from "./EditCard";
 
-function CardPreview({card, handleDelete, deckId}){    
+function CardPreview({handleDelete, deckId, id}){   
+    const [card, setCard] = useState({});
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+    const history = useHistory();
+
+    async function getCard(){
+        try {
+            const response = await readCard(id, signal);
+            setCard(response);
+        } catch(error){
+            if(error.name !== "AbortError") {
+                throw error;
+            }
+        }
+    }
+
+    useEffect(() => {
+        getCard();
+        return () => {
+            abortController.abort();
+        }
+    }, []);
+
+    
     return (
         <div>
-            <p>{card.front}</p>
-            <p>{card.back}</p>
-            <div>
-                <Edit mode="card" deckId={card.deckId} cardId={card.id}/>
-                <Delete mode="card" id={card.id} handleDelete={handleDelete} deckId={deckId}/>
-            </div>
+            <Switch>
+                <Route exact path={`/decks/${deckId}`}>
+                    <p>{card.front}</p>
+                    <p>{card.back}</p>
+                    <div>
+                        <Edit mode="card" deckId={deckId} cardId={card.id}/>
+                        <Delete mode="card" id={card.id} handleDelete={handleDelete} deckId={deckId}/>
+                    </div>
+                </Route>
+                <Route path={`/decks/${deckId}/cards/:cardId/edit`}>
+                    <EditCard card={card} deckId={deckId} />
+                </Route>
+            </Switch>
         </div>
     );
 }
