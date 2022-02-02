22f4b732fecdc66ba7099f4fee25784e11d3ca5c
johmert <johmert@users.noreply.github.com>
2022-02-02 00:28:03 -0800

    button styling

--- src/Layout/components/Buttons.js
+++ src/Layout/components/Buttons.js
@@ -7,48 +7,62 @@ function Buttons({names, deckId, cardId, handleDelete}){
             case "add-card": 
                 buttons.push(
                     <a key={"add-card"} href={`/decks/${deckId}/cards/new`}>
-                        <button>Add Card</button>
+                        <button class="btn btn-info mx-1">Add Card</button>
                     </a>
                 );
                 break;
             case "create-deck": 
                 buttons.push(
                     <a key={"create-deck"} href="/decks/new">
-                        <button>Create Deck</button>
+                        <button class="btn btn-info mb-4">Create Deck</button>
                     </a>
                 );
                 break;
             case "delete-card":
-                buttons.push(<button key={"delete-card"} onClick={() => handleDelete(cardId, deckId)}>Delete</button>);
+                buttons.push(
+                    <button
+                        class="btn btn-danger mx-1" 
+                        key={"delete-card"} 
+                        onClick={() => handleDelete(cardId, deckId)}>
+                            Delete
+                    </button>
+                );
                 break;
             case "delete-deck":
-                buttons.push(<button key={"delete-deck"} onClick={() => handleDelete(deckId)}>Delete</button>);
+                buttons.push(
+                    <button 
+                        class="btn btn-danger mx-1"
+                        key={"delete-deck"} 
+                        onClick={() => handleDelete(deckId)}>
+                            Delete
+                    </button>
+                );
                 break;
             case "edit-card":
                 buttons.push(
                     <a key={"edit-card"} href={`/decks/${deckId}/cards/${cardId}/edit`}>
-                        <button>Edit</button>
+                        <button class="btn btn-warning mx-1">Edit</button>
                     </a>
                 );
                 break;
             case "edit-deck":
                 buttons.push(
                     <a key={"edit-deck"} href={`/decks/${deckId}/edit`}>
-                        <button>Edit</button>
+                        <button class="btn btn-warning mx-1">Edit</button>
                     </a>
                 );
                 break;
             case "study": 
                 buttons.push(
                     <a key={"study"} href={`/decks/${deckId}/study`}>
-                        <button>Study</button>
+                        <button class="btn btn-info mx-1">Study</button>
                     </a>
                 );
                 break;
             case "view": 
                 buttons.push(
                     <a key={"view"} href={`/decks/${deckId}`}>
-                        <button>View</button>
+                        <button class="btn btn-primary mx-1">View</button>
                     </a>
                 );
                 break;
